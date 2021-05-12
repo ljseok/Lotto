634b3e5f570b83fa37702990310eac944415c03b
ccccljs <cho38suk@hamail.net>
2021-05-12 15:33:06 +0900

    Open ConstellationActivity

--- app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -1,11 +1,22 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
+            findViewById<View>(R.id.GoResultButton).setOnClickListener {
+                val intent = Intent(this,ResultActivity::class.java)
+                startActivity(intent)
+            }
+
+      //  Toast.makeText(this,"Consellation Activity", Toast.LENGTH_LONG).show()
+
+
     }
 }
--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Main Activity", Toast.LENGTH_LONG).show()
+
+        findViewById<View>(R.id.constellationCard).setOnClickListener {
+            startActivity(Intent(this,ConstellationActivity::class.java))
+        }
     }
 }
