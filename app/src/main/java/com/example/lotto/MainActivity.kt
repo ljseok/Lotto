2bcf67f47d6c886cc1f11c19ce9530ea37bd0bd7
ccccljs <cho38suk@hamail.net>
2021-05-18 10:47:20 +0900

    openNameActivity

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -17,5 +17,8 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.constellationCard).setOnClickListener {
             startActivity(Intent(this,ConstellationActivity::class.java))
         }
+        findViewById<View>(R.id.nameCard).setOnClickListener {
+            startActivity(Intent(this,Namectivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/lotto/Namectivity.kt
+++ app/src/main/java/com/example/lotto/Namectivity.kt
@@ -1,14 +1,19 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 
 class Namectivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_namectivity)
-
-        Toast.makeText(this,"Name Activity", Toast.LENGTH_LONG).show()
+        findViewById<View>(R.id.GoResultButton).setOnClickListener {
+            val intent = Intent(this,ResultActivity::class.java)
+            startActivity(intent)
+        }
+        //Toast.makeText(this,"Name Activity", Toast.LENGTH_LONG).show()
     }
 }
