b52339b08875e9d3ef06e6a410b1b20c4456b117
ccccljs <cho38suk@hamail.net>
2021-05-26 14:51:39 +0900

    Ball Image Update Function

--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -6,6 +6,8 @@ import android.widget.ImageView
 import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
+    val lottoImageStartId = R.drawable.ball_01
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
@@ -14,14 +16,15 @@ class ResultActivity : AppCompatActivity() {
 
         val result = intent.getIntArrayExtra("result") ?: return
 
-        //if(result == null) {
-          // return;
-       // }
-        val result_sorted = result?.sorted()
+        result?.let {
+            updateLottoBallImage(result.sortedBy { it })
+        }
+    }
+    private fun updateLottoBallImage(result:List<Int>) {
 
-        val lottoImageStartId = R.drawable.ball_01
-       // val lottoImageStartId2 = R.drawable.ball_15
-       // val lottoImageStartId3 = R.drawable.ball_23
+
+        // val lottoImageStartId2 = R.drawable.ball_15(
+        // val lottoImageStartId3 = R.drawable.ball_23
         val imageView18 = findViewById<ImageView>(R.id.imageView18)
         val imageView19 = findViewById<ImageView>(R.id.imageView19)
         val imageView20 = findViewById<ImageView>(R.id.imageView20)
@@ -29,14 +32,12 @@ class ResultActivity : AppCompatActivity() {
         val imageView22 = findViewById<ImageView>(R.id.imageView22)
         val imageView23 = findViewById<ImageView>(R.id.imageView23)
 
-        imageView18.setImageResource(lottoImageStartId+(result[0]-1))
-        imageView19.setImageResource(lottoImageStartId+(result[1]-1))
-        imageView20.setImageResource(lottoImageStartId+(result[2]-1))
-        imageView21.setImageResource(lottoImageStartId+(result[3]-1))
-        imageView22.setImageResource(lottoImageStartId+(result[4]-1))
-        imageView23.setImageResource(lottoImageStartId+(result[5]-1))
-
-
+        imageView18.setImageResource(lottoImageStartId + (result[0] - 1))
+        imageView19.setImageResource(lottoImageStartId + (result[1] - 1))
+        imageView20.setImageResource(lottoImageStartId + (result[2] - 1))
+        imageView21.setImageResource(lottoImageStartId + (result[3] - 1))
+        imageView22.setImageResource(lottoImageStartId + (result[4] - 1))
+        imageView23.setImageResource(lottoImageStartId + (result[5] - 1))
     }
-
 }
+
