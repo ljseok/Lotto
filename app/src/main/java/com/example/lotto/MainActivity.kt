ff1fdebf039e0f9b993a6fd87a7e5658b3db2e06
ccccljs <cho38suk@hamail.net>
2021-05-26 13:26:50 +0900

    RandomNumberGeneration

--- app/src/main/java/com/example/lotto/MainActivity.kt
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.cardview.widget.CardView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +22,37 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.nameCard).setOnClickListener {
             startActivity(Intent(this,Namectivity::class.java))
         }
+
+        fun getRandomLottoNumber(): Int{
+            return Random.nextInt(45)+1
+        }
+
+        fun getRandomLottoNumbers(): MutableList<Int>{
+            val lottoNumbers = mutableListOf<Int>()
+
+            while(true){
+                var numbers: Int = getRandomLottoNumber()
+                var flag_existing: Int = 0
+                if(lottoNumbers.contains(numbers)){
+                    flag_existing =1
+                    break;
+                }
+                if(flag_existing.equals(1))
+                    continue
+                else
+                    lottoNumbers.add(numbers)
+                if(lottoNumbers.size>=0)
+                    break;
+            }
+            return lottoNumbers
+        }
+
+        findViewById<View>(R.id.randomCard).setOnClickListener {
+            val intent = Intent(this,ResultActivity::class.java)
+            intent.putIntegerArrayListExtra("result",ArrayList(getRandomLottoNumbers()))
+
+        }
+
+
     }
 }
