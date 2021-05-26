e15303428ccbdbd6b5746ce3287e15d7ae8592b4
ccccljs <cho38suk@hamail.net>
2021-05-26 14:33:37 +0900

    Display Lotto Balls

--- app/src/main/java/com/example/lotto/ResultActivity.kt
+++ app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
@@ -9,6 +10,33 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        Toast.makeText(this,"Result Activity", Toast.LENGTH_LONG).show()
+        //Toast.makeText(this,"Result Activity", Toast.LENGTH_LONG).show()
+
+        val result = intent.getIntArrayExtra("result") ?: return
+
+        //if(result == null) {
+          // return;
+       // }
+        val result_sorted = result?.sorted()
+
+        val lottoImageStartId = R.drawable.ball_01
+       // val lottoImageStartId2 = R.drawable.ball_15
+       // val lottoImageStartId3 = R.drawable.ball_23
+        val imageView18 = findViewById<ImageView>(R.id.imageView18)
+        val imageView19 = findViewById<ImageView>(R.id.imageView19)
+        val imageView20 = findViewById<ImageView>(R.id.imageView20)
+        val imageView21 = findViewById<ImageView>(R.id.imageView21)
+        val imageView22 = findViewById<ImageView>(R.id.imageView22)
+        val imageView23 = findViewById<ImageView>(R.id.imageView23)
+
+        imageView18.setImageResource(lottoImageStartId+(result[0]-1))
+        imageView19.setImageResource(lottoImageStartId+(result[1]-1))
+        imageView20.setImageResource(lottoImageStartId+(result[2]-1))
+        imageView21.setImageResource(lottoImageStartId+(result[3]-1))
+        imageView22.setImageResource(lottoImageStartId+(result[4]-1))
+        imageView23.setImageResource(lottoImageStartId+(result[5]-1))
+
+
     }
+
 }
